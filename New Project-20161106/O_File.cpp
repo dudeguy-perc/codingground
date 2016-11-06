b43ab0dfb2b01152327238c3a9796a858a2d8c2b
dudeguy-perc <dudeguy.perc@gmail.com>
2016-11-06 02:00:54 -0600

    Update O_File.cpp

--- New Project-20161106/O_File.cpp
+++ New Project-20161106/O_File.cpp
@@ -1,7 +1,8 @@
 #include "O_File.h"
-O_File::O_File(const char* path)
+O_File::O_File(const char* _path)
 {
-    fs.open(path, std::fstream::in | std::fstream::out | std::fstream::app);
+    path = _path;
+    //fs.open(path, std::fstream::in | std::fstream::out | std::fstream::app);
 }
 O_File::~O_File()
 {
@@ -9,15 +10,14 @@ O_File::~O_File()
 }
 void O_File::add(const char* add)
 {
-    if (fs.is_open())
-    {
-        std::cout << "File is open appending: " << add << std::endl;
-    }
+    fs.open(path, std::fstream::out|std::fstream::app);
     fs << add;
+    fs.close();
 }
 void O_File::Display()
 {
     char c;
+    fs.open(path, std::fstream::in);
     while (fs.get(c))
     {
         if (c == '\n')
@@ -31,5 +31,6 @@ void O_File::Display()
         }
         
     }
+    fs.close();
     std::cout << "done." <<std::endl;
 }
