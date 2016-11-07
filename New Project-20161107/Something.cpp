9937982292def3a9ae404867fcb2283d884ebf67
dudeguy-perc <dudeguy.perc@gmail.com>
2016-11-07 01:11:47 -0600

    Created by Coding Ground Client

---
+++ New Project-20161107/Something.cpp
@@ -0,0 +1,9 @@
+#include "Something.h"
+
+unsigned int Something::count = 0;
+/*Something::Something()
+{
+In file included from Something.cpp:1:0:                                                                                                                                 
+Something.h:6:5: note: 'Something::Something()' previously defined here                                                                                                  
+     Something():id(count++){};
+}*/
