13137b6270e7f559075d9e29501f2c4eb72c2a6f
dudeguy-perc <dudeguy.perc@gmail.com>
2016-11-07 01:11:20 -0600

    Created by Coding Ground Client

---
+++ New Project-20161107/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Something.h"
+//using std::cout;
+//using std::endl;
+
+int main()
+{
+    Something s1;
+    Something s2;
+   std::cout << s1.id << " " << s2.id << std::endl;
+   
+   return 0;
+}
+
